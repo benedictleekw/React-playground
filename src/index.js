4bb7a7d915c9dbb47b2773d8517b23c05355e972
benedictleekw <benedictleekw@gmail.com>
2021-09-29 21:56:55 -0500

    crete book component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "hello world")
+// }
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/41j2aGV1ujL._SY346_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => <h1>The Eye of the World</h1>;
+const Author = () => <h4>ROBERT JORDAN</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
